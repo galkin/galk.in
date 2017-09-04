cae14ff84de9e993226dcd8f7ac3ec9f6e1c7aab
Nikita Galkin <nikita@galk.in>
2017-09-04 16:09:17 +0300

    Sigme JS Meetup Slides

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ export default class SubPage extends Component {
   render() {
     return (
       <Iframe
-        url="https://docs.google.com/presentation/d/19UjSGLl7_4oKVm5d8Rj_YCiAEmiRVVKlwcFZmSP-x6Y/embed?start=false&loop=false&delayms=3000"
+        url="https://docs.google.com/presentation/d/19AVBpacfT4ICAk8BMcn3Titj2_uh7dYhqpu0AQY5oYI/embed?start=false&loop=false&delayms=3000"
         frameborder="0"
         width="100%"
         height="100%"
