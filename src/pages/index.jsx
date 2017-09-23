9e45ecf0cad3e69925000ab3c71aa0e6f0c299c2
Nikita Galkin <nikita@galk.in>
2017-09-23 06:37:34 +0300

    QAfest

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ export default class SubPage extends Component {
   render() {
     return (
       <Iframe
-        url="https://docs.google.com/presentation/d/1MQWfgwL8ccMte1SHR4utYrbu4QG2wqGbRE6kZ9sFfXU/embed?start=false&loop=false&delayms=3000"
+        url="https://docs.google.com/presentation/d/1giuLDtVSS_kdx0O9ELVQhz7JQj_wytMRUhHTZu4sYuw/embed?start=false&loop=false&delayms=3000"
         frameborder="0"
         width="100%"
         height="100%"
