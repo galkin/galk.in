0b58b27b72750cae54a8219db158ba122379a2b4
Nikita Galkin <nikita@galk.in>
2017-10-28 11:03:09 +0300

    PMday

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ export default class SubPage extends Component {
   render() {
     return (
       <Iframe
-        url="https://docs.google.com/presentation/d/1MA3S3aNihyKdHCzu_yQ0wN0ud_M52tzoWmTDBwMKDUw/embed?start=false&loop=false&delayms=3000"
+        url="https://docs.google.com/presentation/d/e/2PACX-1vRqlVMUe1u6A63mIdWBaWosFSSExfGPr5UpH60lD5LP-Aho3RQINjArbAkWaeLk2mJ2qbpleyy95RSP/embed?start=false&loop=false&delayms=3000"
         frameborder="0"
         width="100%"
         height="100%"
